87bdcf821442f7d5373a9f058f9d5810652f2a04
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-04 11:56:26 -0300

    add array de listas

--- src/js/dados.js
+++ src/js/dados.js
@@ -1,1 +1,23 @@
-let dados = "Hyundai"
+let dados = [
+
+    {
+
+        titulo: "Hyundai",
+        descricao: "O Hyundai HB20 2023 é um hatch compacto muito popular no Brasil, conhecido por seu design moderno e tecnologia embarcada. A linha 2023 traz melhorias estéticas, principalmente na grade frontal e nos faróis, além de atualizações nos itens de conforto e segurança.O carro oferece uma variedade de versões com motorização que vai desde o 1.0 aspirado até o 1.0 turbo, com opções de câmbio manual e automático.",
+        link: "https://www.webmotors.com.br/hyundai/hb20/2023"
+
+    },
+
+    {
+        titulo: "Ford Ranger",
+        descricao: "A Ford Ranger 2024 é uma picape de médio porte com design robusto e moderno, oferecendo uma cabine espaçosa e várias opções de motorização. O modelo traz melhorias na tecnologia e conforto, ideal para quem busca desempenho tanto em estrada quanto fora dela.",
+        link: "https://www.webmotors.com.br/ford/ranger/2024"
+    },
+
+    {
+        titulo: "Audi Q3",
+        descricao: "O Audi Q3 2024 é um SUV compacto que combina elegância e desempenho. Com um design sofisticado e um interior luxuoso, o Q3 oferece tecnologia avançada e um excelente conforto, sendo uma ótima opção para quem busca estilo e praticidade.",
+        link: "https://www.webmotors.com.br/audi/q3/2024"
+    }
+
+];
