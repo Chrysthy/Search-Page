48a503b537a27d16397286aa9c00d15c82a17a69
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-05 09:05:08 -0300

    add section.innerHTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-let section = document.getElementById('resultados-pesquisa')
+let section = document.getElementById('resultados-pesquisa');
+
+section.innerHTML = `
+
+            <div class="item-resultado">
+
+                <h2>
+
+                    <a href="#" target="_blank">Hyundai</a>
+
+                </h2>
+
+                <p class="descricao-meta">O Hyundai HB20 2023 é um hatch compacto muito popular no Brasil, conhecido por
+                    seu design moderno e tecnologia embarcada. A linha 2023 traz melhorias estéticas, principalmente na
+                    grade frontal e nos faróis, além de atualizações nos itens de conforto e segurança. O carro oferece
+                    uma variedade de versões com motorização que vai desde o 1.0 aspirado até o 1.0 turbo, com opções de
+                    câmbio manual e automático.</p>
+
+                <a href="https://www.webmotors.com.br/hyundai/hb20/2023" target="_blank">Mais informações</a>
+
+            </div>
+
+
+`
