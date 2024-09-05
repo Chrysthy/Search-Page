8dd7998d14c540d92896c26ac85084db145ba56d
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-05 09:15:29 -0300

    add for of

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,24 @@
 let section = document.getElementById('resultados-pesquisa');
 
-section.innerHTML = `
+for (let dado of dados) {
 
-            <div class="item-resultado">
+    section.innerHTML += `
+    
+                <div class="item-resultado">
+    
+                    <h2>
+    
+                        <a href="#" target="_blank">${dado.titulo}</a>
+    
+                    </h2>
+    
+                    <p class="descricao-meta">${dado.descricao}</p>
+    
+                    <a href="${dado.link}" target="_blank">Mais informações</a>
+    
+                </div>
+    
+    
+    `
+}
 
-                <h2>
-
-                    <a href="#" target="_blank">${dados[0].titulo}</a>
-
-                </h2>
-
-                <p class="descricao-meta">O Hyundai HB20 2023 é um hatch compacto muito popular no Brasil, conhecido por
-                    seu design moderno e tecnologia embarcada. A linha 2023 traz melhorias estéticas, principalmente na
-                    grade frontal e nos faróis, além de atualizações nos itens de conforto e segurança. O carro oferece
-                    uma variedade de versões com motorização que vai desde o 1.0 aspirado até o 1.0 turbo, com opções de
-                    câmbio manual e automático.</p>
-
-                <a href="https://www.webmotors.com.br/hyundai/hb20/2023" target="_blank">Mais informações</a>
-
-            </div>
-
-
-`
